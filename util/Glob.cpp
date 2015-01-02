7b383fbd48bc87e719e45d9003bbb4db758c83ec
Touched <Touched@users.noreply.github.com>
2015-01-03 01:19:26 +0200

    Started util classes

---
+++ util/Glob.cpp
@@ -0,0 +1,2 @@
+#include "Glob.hpp"
+
---
+++ util/Glob.hpp
@@ -0,0 +1,13 @@
+#ifndef WYNAUT_UTIL_GLOB_HPP_
+#define WYNAUT_UTIL_GLOB_HPP_
+
+namespace util {
+    class Glob {
+    public:
+        Glob();
+
+        ~Glob();
+    };
+}
+
+#endif /* WYNAUT_UTIL_GLOB_HPP_ */
